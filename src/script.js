7e465450cfdd8bef26d9a047399964922f69811b
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2021-06-21 14:51:57 +0200

    Changed to OOP, Cells are in 2D-Array

--- src/script.js
+++ src/script.js
@@ -1,10 +1,11 @@
-const canvasWidth = window.innerWidth;
-const canvasHeight = window.innerHeight;
+const canvasWidth = 500;
+const canvasHeight = 500;
 
 const cellSize = 10;
 
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
+
     generateField();
 }
   
@@ -12,11 +13,38 @@ function draw() {
 }
 
 function generateField(){
-    for (let i = 0; i <= canvasHeight; i += cellSize) {
-        for (let k = 0; k <= canvasWidth; k += cellSize) {
-            fill(Math.round(Math.random()*255));
-            rect(k, i, cellSize, cellSize);            
+    let field = new Field(canvasWidth, canvasHeight, cellSize);
+    console.log(field.cellArray[1][1]);
+}
+
+function Field(width, height, cellSize){
+    this.cellArray = [];
+
+    this.init = function() {
+        let horizontalCellCount = Math.floor(width/cellSize);
+        let verticalCellCount = Math.floor(height/cellSize);
+
+        for (let i = 0; i < verticalCellCount; i++) {
+            this.cellArray[i] = [];
+            for (let k = 0; k < horizontalCellCount; k ++) {
+                this.cellArray[i][k] = (new Cell(k, i, cellSize, Math.round(Math.random()*255)));
+            }
         }
+    }
+
+    this.init();
+
+}
+
+function Cell(x, y, cellSize, color){
+    this.x = x;
+    this.y = y;
+    this.cellSize = cellSize;
+    
+    fill(color);
+    rect(x*cellSize, y*cellSize, cellSize, cellSize);
+
+    this.update = function() {
         
     }
 }
