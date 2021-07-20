32842f764292c95a2407b04b3c46b7c87a0119c2
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2021-07-20 14:07:43 +0200

    bugged: aliveNeighbours all have to be shifted one pos down

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-const canvasWidth = innerWidth * 2;
-const canvasHeight = innerHeight * 2;
+const canvasWidth = 500;
+const canvasHeight = 500;
 
 const cellSize = 10;
 let paintable = true;
@@ -16,6 +16,10 @@ function setup() {
         //toggles paint
         paintable = !paintable;
     });
+
+    
+    
+    //ellipse(10, 10, 100);
 }
 
 function draw() {
@@ -25,16 +29,38 @@ function draw() {
 
         let drawCell = field.cellArray[drawY][drawX];
 
-        if (drawCell == undefined) {
+        if (typeof drawCell === 'undefined') {
+            return;
+        } else if (drawCell.alive === true) {
             return;
         } else {
-            field.cellArray[drawY][drawX].changeState();
+            drawCell.changeState();
         }
     }
 }
 
+function mouseClicked() {
+    if (mouseX < 0 || mouseY < 0) {
+        return;
+    }
+    if (mouseX > canvasWidth || mouseY > canvasHeight) {
+        return;
+    }
+
+    let drawX = Math.floor(mouseX / cellSize);
+    let drawY = Math.floor(mouseY / cellSize);
+
+    let drawCell = field.cellArray[drawY][drawX];
+    console.log(drawCell.alive);
+}
+
 function generateField() {
     field = new Field(canvasWidth, canvasHeight, cellSize);
+    field.init();
+
+    document.getElementById("update").addEventListener("click", function () {
+        field.update();
+    });
 }
 
 function Field(width, height, cellSize) {
@@ -47,28 +73,141 @@ function Field(width, height, cellSize) {
         for (let i = 0; i < verticalCellCount; i++) {
             this.cellArray[i] = [];
             for (let k = 0; k < horizontalCellCount; k++) {
-                this.cellArray[i][k] = (new Cell(k, i, cellSize, true));
+                this.cellArray[i][k] = (new Cell(k, i, cellSize, false, false));
             }
         }
     }
 
-    this.init();
+    //this.init();
+
+    this.update = function () {
+
+        for (let i = 0; i < this.cellArray.length; i++) {
+            for (let k = 0; k < this.cellArray[i].length; k++) {
+                const cell = this.cellArray[i][k];
+
+                let aliveNeighbours = 0;
+
+                for (let j = -1; j <= 1; j++) {
+                    for (let l = -1; l <= 1; l++) {
+                        if (i+j < 0 || l+k < 0) {
+                            continue;
+                        }
+                        if (i+j > this.cellArray.length-1 || l+k > this.cellArray[i].length-1) {
+                            continue;
+                        }
+
+                        aliveNeighbours += this.cellArray[i+j][l+k].alive;
+                    }
+                }
+
+                aliveNeighbours -= this.cellArray[i][k].alive;
+
+                fill(255,0,0);
+                text(aliveNeighbours, cell.x * cell.cellSize, cell.y * cell.cellSize);
+
+
+                if (cell.alive) {
+                    //cell lives
+                    //next gen -->
+                    if (aliveNeighbours == 2 || aliveNeighbours == 3) {
+                        cell.futureState = true;
+                    } else {
+                        cell.futureState = false;
+                    }
+                } else {
+                    //cell is dead
+                    //next gen -->
+                    if (aliveNeighbours == 3) {
+                        cell.futureState = true;
+                    } else {
+                        cell.futureState = false;
+                    }
+                }
+            }
+        }
+
+        /*for (let i = 0; i < this.cellArray.length; i++) {
+            for (let k = 0; k < this.cellArray[i].length; k++) {
+                const cell = this.cellArray[i][k];
+
+                if (cell.futureState) {
+                    fill(0);
+                    rect(cell.x * cell.cellSize, cell.y * cell.cellSize, cell.cellSize, cell.cellSize);
+                } else {
+                    fill(255);
+                    rect(cell.x * cell.cellSize, cell.y * cell.cellSize, cell.cellSize, cell.cellSize);
+                }
+
+                cell.alive = cell.futureState;
+            }
+        }*/
+    }
+
+    this.getAliveNeighbours = function (x, y) {
+        let aliveCells = 0;
+
+        //x:2 , y:2
+        //1, 2, 3
+        for (let i = x - 1; i < x+2; i++) {
+            for (let k = y - 1; k < y+2; k++) {
+                //OutOfBounds
+                if (i < 0 || k < 0) {
+                    continue;
+                }
+                if (i > this.cellArray.length-1 || k > this.cellArray[i].length-1) {
+                    continue;
+                }
+                if (i == x && k == y) {
+                    continue;
+                }
+
+                if (this.cellArray[i][k] == undefined) {
+                    console.warn("Undefined");
+                }
+                
+                if (this.cellArray[i][k].alive) {
+                    aliveCells++;
+                }
+            }
+        }
+
+        return aliveCells;
+    }
 
 }
 
-function Cell(x, y, cellSize, alive) {
+function Cell(x, y, cellSize, alive, futureState) {
     this.x = x;
     this.y = y;
     this.cellSize = cellSize;
     this.alive = alive;
+    this.futureState = futureState;
 
-    noStroke();
-    fill(alive * 255);
-    rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    this.init = function () {
+        noStroke();
+        fill(!alive * 255);
+        rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+    this.init();
 
     this.changeState = function () {
-        alive = !alive;
-        fill(alive * 1);
-        rect(x * cellSize, y * cellSize, cellSize, cellSize);
+        //not redrawing correctly
+
+
+        this.alive = !this.alive;
+
+
+        this.updateColor(this.alive);
+    }
+
+    this.updateColor = function (alive) {
+        if (alive) {
+            fill(0);
+        } else {
+            fill(255);
+        }
+
+        rect(this.x * this.cellSize, this.y * this.cellSize, this.cellSize, this.cellSize);
     }
 }
