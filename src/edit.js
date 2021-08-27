d94297531fddd1e2cb4a426c049f477fce7f3680
spasskopf <78727783+spasskopf@users.noreply.github.com>
2021-08-27 17:42:39 +0200

    Add semicolons and fix a few typos

--- src/edit.js
+++ src/edit.js
@@ -26,25 +26,25 @@ const formFunction = new ActivatingSpecialFunction('f', () => {
             element.highlight();
         }
     }
-})
+});
 
-const lineFunction = new ToggleableSpecialFunction('l', "Line", (xStart, yStart, xEnd, yEnd) => drawLine(xStart, yStart, xEnd, yEnd))
+const lineFunction = new ToggleableSpecialFunction('l', "Line", (xStart, yStart, xEnd, yEnd) => drawLine(xStart, yStart, xEnd, yEnd));
 
 /**
  * Special Functions that have a keybinding
  * For example lines, forms, ...
  * @type {ToggleableSpecialFunction[]|ActivatingSpecialFunction[]}
  */
-const functions = [formFunction, lineFunction]
+const functions = [formFunction, lineFunction];
 
 /**
  * Returns true if the highlighted cells should be recalculated and redrawn,
- * false otherwiese
+ * false otherwise
  * @returns {boolean} whether or not to clear the highlighted cells
  */
 function defaultShouldClearHighlight() {
-    const lastCell = getCellFromScreenPosition(pmouseY, pmouseX)
-    const currentCell = getCellFromScreenPosition(mouseY, mouseX)
+    const lastCell = getCellFromScreenPosition(pmouseY, pmouseX);
+    const currentCell = getCellFromScreenPosition(mouseY, mouseX);
     return !(lastCell.y === currentCell.y && lastCell.x === currentCell.x);
 }
 
@@ -55,17 +55,17 @@ function defaultShouldClearHighlight() {
  * @param {Function} drawFunction a function that draws this element
  */
 function ActivatingSpecialFunction(keyBind, drawFunction) {
-    this.keyBind = keyBind
-    this.drawFunction = drawFunction
+    this.keyBind = keyBind;
+    this.drawFunction = drawFunction;
     this.update = () => {
-        this.drawFunction()
-    }
+        this.drawFunction();
+    };
     /**
-     * Returns wether or not to clear highlights
+     * Returns whether or not to clear highlights
      */
     this.shouldClearHighlights = () => {
-        return defaultShouldClearHighlight()
-    }
+        return defaultShouldClearHighlight();
+    };
 }
 
 /**
@@ -76,46 +76,46 @@ function ActivatingSpecialFunction(keyBind, drawFunction) {
  * @param {Function}drawFunction a function that takes the mouse positions and draws this function
  */
 function ToggleableSpecialFunction(keyBind, name, drawFunction) {
-    this.keyBind = keyBind
-    this.drawFunction = drawFunction
+    this.keyBind = keyBind;
+    this.drawFunction = drawFunction;
     /**
      * Is this function currently highlighting anything / is it active
      * @type {Boolean}
      */
-    this.highlighting = false
-    this.xStart = undefined
-    this.yStart = undefined
+    this.highlighting = false;
+    this.xStart = undefined;
+    this.yStart = undefined;
     /**
      * User-friendly name of this function
      * @type {String}
      */
-    this.name = name
+    this.name = name;
 
     /**
      * Updates the highlighting value
      */
     this.update = () => {
-        this.highlighting = !this.highlighting
+        this.highlighting = !this.highlighting;
 
         if (this.highlighting) {
-            this.xStart = mouseX
-            this.yStart = mouseY
+            this.xStart = mouseX;
+            this.yStart = mouseY;
         } else {
-            makeHighlightsPermanent()
+            makeHighlightsPermanent();
         }
-    }
+    };
 
     this.mouseEvent = () => {
         if (this.highlighting) {
-            this.drawFunction(this.xStart, this.yStart, mouseX, mouseY)
+            this.drawFunction(this.xStart, this.yStart, mouseX, mouseY);
         }
-    }
+    };
     /**
      * Returns whether or not to clear highlights
      */
     this.shouldClearHighlights = () => {
-        return defaultShouldClearHighlight()
-    }
+        return defaultShouldClearHighlight();
+    };
 }
 
 
@@ -123,7 +123,7 @@ function keyTyped() {
     // activate line drawing
     for (let specialFunction of functions) {
         if (specialFunction.keyBind === key) {
-            specialFunction.update()
+            specialFunction.update();
         }
     }
 }
@@ -131,27 +131,27 @@ function keyTyped() {
 function mousePressed() {
     for (let specialFunction of functions) {
         if (specialFunction.mouseEvent) {
-            specialFunction.mouseEvent()
+            specialFunction.mouseEvent();
         }
     }
 }
 
 function mouseDragged() {
-    let overwrittenBySpecialFunction = false
+    let overwrittenBySpecialFunction = false;
     for (let specialFunction of functions) {
         if (specialFunction.highlighting) {
-            specialFunction.mouseEvent()
-            overwrittenBySpecialFunction = true
+            specialFunction.mouseEvent();
+            overwrittenBySpecialFunction = true;
         }
     }
     if (!overwrittenBySpecialFunction) {
         if (paintable) {
-            const drawCell = getCellFromScreenPosition(mouseY, mouseX)
+            const drawCell = getCellFromScreenPosition(mouseY, mouseX);
             if (typeof drawCell !== 'undefined') {
                 // - LMB to draw
                 // - RMB to erase
-                drawCell.alive = mouseButton !== LEFT
-                drawCell.changeState()
+                drawCell.alive = mouseButton !== LEFT;
+                drawCell.changeState();
             }
 
         }
@@ -164,65 +164,65 @@ function draw() {
         highlightedCells.forEach((cell) => {
             cell.cancelHighlight();
             highlightedCells = [];
-        })
+        });
     }
 
     //Displays the active functions in the paint button
     const activeFunctionsText = functions.filter((specFunction) => specFunction.highlighting)
         .map((specFunction) => specFunction.name)
-        .join(" ")
-    const paint = document.getElementById("paint")
+        .join(" ");
+    const paint = document.getElementById("paint");
     const text = `paint<br>${activeFunctionsText}`;
     if (paint.innerHTML !== text) {
-        paint.innerHTML = text
+        paint.innerHTML = text;
     }
 }
 
 function drawLine(xStart, yStart, xEnd, yEnd) {
-    let startCell = getCellFromScreenPosition(yStart, xStart)
-    let endCell = getCellFromScreenPosition(yEnd, xEnd)
-    const minCellY = Math.floor(Math.min(yStart, yEnd) / cellSize)
-    const maxCellY = Math.floor(Math.max(yStart, yEnd) / cellSize)
+    let startCell = getCellFromScreenPosition(yStart, xStart);
+    let endCell = getCellFromScreenPosition(yEnd, xEnd);
+    const minCellY = Math.floor(Math.min(yStart, yEnd) / cellSize);
+    const maxCellY = Math.floor(Math.max(yStart, yEnd) / cellSize);
     if (startCell.x > endCell.x) {
-        [startCell, endCell] = [endCell, startCell]
+        [startCell, endCell] = [endCell, startCell];
     }
 
-    const deltaY = endCell.y - startCell.y
-    const deltaX = endCell.x - startCell.x
+    const deltaY = endCell.y - startCell.y;
+    const deltaX = endCell.x - startCell.x;
 
     if (deltaX === 0) {
         for (let cellY = minCellY; cellY <= maxCellY; cellY++) {
-            highlight(getCellFromIndex(cellY, startCell.x))
+            highlight(getCellFromIndex(cellY, startCell.x));
         }
     } else {
-        const yIncrementPerCell = deltaY / deltaX
+        const yIncrementPerCell = deltaY / deltaX;
         for (let x = 0, y = startCell.y; x <= deltaX; x++, y += yIncrementPerCell) {
             //Simple: not more than one cell on the y axis
             if (Math.abs(yIncrementPerCell) <= 1) {
-                highlight(getCellFromIndex(Math.floor(y), startCell.x + x))
+                highlight(getCellFromIndex(Math.floor(y), startCell.x + x));
             } else {
-                let start = y - yIncrementPerCell
-                let end = y
+                let start = y - yIncrementPerCell;
+                let end = y;
                 if (start > end) {
-                    [start, end] = [end, start]
+                    [start, end] = [end, start];
                 }
                 for (let cellY = start; cellY <= end && cellY >= minCellY && cellY <= maxCellY; cellY++) {
-                    highlight(getCellFromIndex(Math.floor(cellY), startCell.x + x))
+                    highlight(getCellFromIndex(Math.floor(cellY), startCell.x + x));
                 }
             }
         }
     }
 
-    highlight(startCell)
-    highlight(endCell)
+    highlight(startCell);
+    highlight(endCell);
 }
 
 /**
  * @param {Cell} cell the cell to highlight
  */
 function highlight(cell) {
-    cell.highlight()
-    highlightedCells.push(cell)
+    cell.highlight();
+    highlightedCells.push(cell);
 }
 
 /**
@@ -244,9 +244,9 @@ function getCellFromScreenPosition(yPos, xPos) {
 function getCellFromIndex(yPos, xPos) {
     const col = field.cellArray[yPos];
     if (col) {
-        return col[xPos]
+        return col[xPos];
     } else {
-        return undefined
+        return undefined;
     }
 }
 
